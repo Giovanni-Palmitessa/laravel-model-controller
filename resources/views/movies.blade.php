3cf32302f9f347c22984ec4001131a43c16c900d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-26 16:32:54 +0200

    add date in card

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -11,6 +11,7 @@
                   <h5 class="card-title">{{ $movie->title }}</h5>
                   <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
                   <p class="card-text">Nazionalità: {{ $movie->nationality }}</p>
+                  <p class="card-text">Data di Uscita: {{ $movie->date }}</p>
                   <a href="#" class="card-link">Card link</a>
                   <a href="#" class="card-link">Another link</a>
                 </div>
