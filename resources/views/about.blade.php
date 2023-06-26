a4274239ceaadf4b2bea47d545059d91ca4caceb
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-26 16:58:54 +0200

    add style for about us section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.base')
 
 @section('contents')
-    <h2>About Us</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quis delectus soluta quae alias, magni maiores odit necessitatibus? Architecto corporis assumenda possimus hic corrupti, ut dolorem porro repellat enim id.</p>
+    <h2 class="about-title my-4">About Us</h2>
+    <p class="about-paragraph text-center">Guarda tutti i film che vuoi Gratis! Di addio alle vecchie piattaforme di streamig. Con Boolean Movies hai tutti i film di cui hai bisogno!</p>
 @endsection
