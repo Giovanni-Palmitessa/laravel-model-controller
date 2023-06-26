702316613db1ee26f040b6dfcefa516c003641d5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-26 15:30:09 +0200

    add methods in controller for homepage, about

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('homepage');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    // public function movies()
+    // {
+    //     $arrMovies = Movie::all();;
+    //     return view('movies', compact('arrMovies'));
+    // }
 }
