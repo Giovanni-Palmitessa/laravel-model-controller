b8a87264dfdbe98162380e56c08c6e861a0b5572
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-26 15:25:59 +0200

    add route for index

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::get('/about', [PageController::class, 'about'])->name('about');
