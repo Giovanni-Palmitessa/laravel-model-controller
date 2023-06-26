ccbeb21fefdb37b3d34cdddb0fc1bbb1e7e900ca
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-26 16:54:20 +0200

    add style to homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.base')
 
 @section('contents')
-    <h2>Benvenuti su Boolean Movies</h2>
+    <h2 class="home-title text-center my-4">Benvenuti su Boolean Movies</h2>
+    <p class="home-paragraph text-center">Clicca su Movies List e inizia a cercare il tuo film!</p>
 @endsection
