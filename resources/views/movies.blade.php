6fce0cdef43df6447c14cbd543e4b9e99364a17a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-26 17:01:21 +0200

    add style to movies section

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.base')
 
 @section('contents')
-    <h2>Ecco la lista dei film:</h2>
+    <h2 class="my-4">Ecco la lista dei film:</h2>
 
     <div class="row g-3 row-cols-3">
         @foreach ($arrMovies as $movie)
-        <div class="col">
+        <div class="col mb-5">
             <div class="card h-100">
                 <div class="card-body">
                   <h5 class="card-title">{{ $movie->title }}</h5>
